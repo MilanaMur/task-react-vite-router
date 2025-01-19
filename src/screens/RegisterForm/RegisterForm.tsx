f065fff75db39a2489fa969510947b2f580f6247
MilanaMur <milana.murashkina@yandex.ru>
2025-01-19 19:10:12 +0300

    Реализован RegisterForm

--- src/screens/RegisterForm/RegisterForm.tsx
+++ src/screens/RegisterForm/RegisterForm.tsx
@@ -1,6 +1,33 @@
+import clsx from 'clsx';
+import styles from './RegisterForm.module.scss';
 import { FormProps } from '../../containers/Form/Form';
+import { Title } from '@/ui';
 
 export type RegisterFormProps = {
 	className?: string;
 	formSettings?: Omit<FormProps, 'children'>;
 };
+
+export function RegisterForm({ className, formSettings }: RegisterFormProps) {
+	return (
+		<Form
+			{...formSettings}
+			method="post"
+			className={clsx(styles.container, className)}
+		>
+			<Title>Зарегистрироваься</Title>
+			<Field>
+				<InputText name="name" placeholder="Имя" />
+			</Field>
+			<Field>
+				<InputEmail name="email" placeholder="email@example.com" />
+			</Field>
+			<Field>
+				<InputPassword name="password" placeholder="Пароль" />
+			</Field>
+			<Field>
+				<InputPassword placeholder="Повторите пароль" />
+			</Field>
+		</Form>
+	);
+}

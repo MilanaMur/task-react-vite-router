a0b9a89df4f5a16b454fedfe8763890225066403
MilanaMur <milana.murashkina@yandex.ru>
2025-01-19 18:25:33 +0300

    Реализован Form

--- src/containers/Form/Form.tsx
+++ src/containers/Form/Form.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import styles from './Form.module.scss';
 import React, { FormHTMLAttributes, FunctionComponent } from 'react';
 
 export type FormTag = 'form' | FunctionComponent;
@@ -7,3 +9,16 @@ export type FormProps = {
 	Tag?: FormTag;
 	children?: React.ReactNode;
 } & FormHTMLAttributes<HTMLFormElement>;
+
+export function Form({
+	className,
+	children,
+	Tag = 'form',
+	...props
+}: FormProps) {
+	return (
+		<Tag {...props} className={clsx(styles.container, className)}>
+			{children}
+		</Tag>
+	);
+}

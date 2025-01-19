efad2a5e78ad5386dbe2e5aefececa71989f8b6e
MilanaMur <milana.murashkina@yandex.ru>
2025-01-19 18:36:33 +0300

    Реализован Field

--- src/containers/Field/Field.tsx
+++ src/containers/Field/Field.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import styles from './Field.module.scss';
 import { ReactNode } from 'react';
 
 export type FieldProps = {
@@ -5,3 +7,12 @@ export type FieldProps = {
 	children?: ReactNode;
 	label?: string;
 };
+
+export function Field({ className, children, label }: FieldProps) {
+	return (
+		<label className={clsx(styles.container, className)}>
+			{label && <span className={clsx(styles.label)}>{label}</span>}
+			{children}
+		</label>
+	);
+}

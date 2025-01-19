ac81062f0161319db1c1f3b59e8bd1fe7a000b3a
MilanaMur <milana.murashkina@yandex.ru>
2025-01-19 15:53:32 +0300

    Реализован Anchor

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -1,4 +1,5 @@
 import { AnchorHTMLAttributes, MouseEvent } from 'react';
+import { useAnchor, useLinkProps } from './hooks';
 
 export type LinkValue =
 	| string
@@ -8,3 +9,14 @@ export type AnchorProps = Omit<
 	AnchorHTMLAttributes<HTMLAnchorElement>,
 	'link'
 > & { link?: LinkValue };
+
+export function Anchor({ href, children, ...props }: AnchorProps) {
+	const LinkElement = useAnchor(href);
+	const propsLink = useLinkProps(href);
+
+	return (
+		<LinkElement {...props} {...propsLink}>
+			{children}
+		</LinkElement>
+	);
+}

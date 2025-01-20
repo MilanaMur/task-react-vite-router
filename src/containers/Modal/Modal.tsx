594278ec2c44d82bf0293b83d6a93e26a4543d9c
MilanaMur <milana.murashkina@yandex.ru>
2025-01-20 19:55:49 +0300

    Реализован Modal

--- src/containers/Modal/Modal.tsx
+++ src/containers/Modal/Modal.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, ReactElement } from 'react';
+import clsx from 'clsx';
+import styles from './Modal.module.scss';
+import { ReactNode, ReactElement, useState } from 'react';
 
 export type ModalProps = {
 	className?: string;
@@ -6,7 +8,39 @@ export type ModalProps = {
 	onClose?: () => void;
 };
 
+export function Modal({ className, children, onClose }: ModalProps) {
+	return (
+		<div className={clsx(styles.modal, className)}>
+			<div className={clsx(styles.content)}>
+				<button className={clsx(styles.close)} onClick={onClose}>
+					&times;
+				</button>
+				{children}
+			</div>
+		</div>
+	);
+}
+
 export type TriggerProps = {
 	children?: ReactElement;
 	modal?: ReactElement;
 };
+
+export function Trigger({ children, modal }: TriggerProps) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
+
+	return (
+		<>
+			{children && <div onClick={handleOpenModal}>{children}</div>}
+			{isModalOpen && <Modal onClose={handleCloseModal}>{modal}</Modal>}
+		</>
+	);
+}
